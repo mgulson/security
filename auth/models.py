f59063c9fdc548a4107db1841d6309e2771fbca6
Michael Gulson <msgulson@gmail.com>
2025-03-31 18:17:03 -0500

    add user table

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password_hash = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    
